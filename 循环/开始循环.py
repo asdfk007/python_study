13b9dc5d0cfba14b330366e9e0fe6356309f883a
asdfk007 <asdfk0071@hotmail.com>
2023-07-14 13:26:31 +0800

    我学习python的笔记

---
+++ 循环/开始循环.py
@@ -0,0 +1,10 @@
+# 假设你有一个女朋友，有一天你惹你女朋友生气了，她要求你说100遍老婆我错了
+
+number_of_apologies = int(input('请输入道歉次数：'))
+i = 0
+while i < number_of_apologies:
+    print('老婆我错了')
+    i += 1
+
+
+# 定义两个变量，i，result
