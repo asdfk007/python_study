cd6e0c47374f387fa055c47f329c13a1fb00d638
asdfk007 <asdfk0071@hotmail.com>
2023-07-14 15:01:37 +0800

    我学习python的笔记

---
+++ 循环/for循环2.py
@@ -0,0 +1,6 @@
+str1 = 'abcdefg'
+for i in str1:
+    if i == "c":
+        print("不打印")
+        continue
+    print(i)
---
+++ 循环/尝试使用for语句.py
@@ -0,0 +1,8 @@
+# 计算1-10的累加和，8不做加
+# 给定变量r
+r = 0
+# for 临时变量 in 序列
+
+for i in range(11):
+    r += i
+print(r)
